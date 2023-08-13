e53b025885db3d4eeab1407464fae4e18757fca3
Sbsbsv <sbsbsv002@gmail.com>
2023-08-13 18:06:39 +0530

    dontknow

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,10 +1,24 @@
 const mongoose=require("mongoose");
-
-
 const connectDatabase=()=>{
-mongoose.set('strictQuery', false); // Suppress the warning
-mongoose.connect(process.env.DB_URI,{useNewUrlParser:true,useUnifiedTopology:true}).then((data)=>{
-    console.log(`Connected to db with server at ${data.connection.host}`);
-})
+// mongoose.set('strictQuery', false); // Suppress the warning
+// mongoose.connect(process.env.DB_URI).then((data)=>{
+//     console.log(`Connected to db with server at ${data.connection.host}`);
+// })
+
+console.log(process.env.DB_URI)
+
+const connectionParams={
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true 
+}
+mongoose.connect(process.env.DB_URI)
+    .then( () => {
+        console.log('Connected to database ')
+    })
+    .catch( (err) => {
+        console.error(`Error connecting to the database. \n${err}`);
+    })
+
 }
 module.exports=connectDatabase
